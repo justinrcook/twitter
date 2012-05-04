a1d73c955bc96e3f3b90d19bd9532d289faece55
Justin Cook <justinrcook@gmail.com>
2012-05-04 15:49:23 -0500

    Added description and location. Callback to grab information from Twitter before creating Person.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,21 @@
 class Person < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :avatar, :last_checked, :name, :username
+  attr_accessible :avatar, :last_checked, :name, :username, :description, :location
 
 	# Validations
 	validates_presence_of :username
+
+	# Callbacks
+	before_create :get_profile_information
+	#after_create :update_timeline
+
+	# Grab the Person's profile information from Twitter
+	def get_profile_information
+		user = Twitter.user(self.username)
+		self.name					= user.name
+		self.avatar				= user.profile_image_url
+		self.description	= user.description
+		self.location			= user.location
+	end
+
 end
