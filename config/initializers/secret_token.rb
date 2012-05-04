06a2f4987e116600df4c51830394adfe6979c863
Justin Cook <jcook@aea267.k12.ia.us>
2012-05-04 11:18:12 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = 'cda62f259704bfb2d30e572423183a352d8a55ee05dc4940cde3b2dbe93eaf8ff8e11f80523091680aa4e274656dde672637ecfe793fe93886eeb39561cee2a7'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Twitter::Application.routes.draw do
+  root :to => "home#index"
+end
